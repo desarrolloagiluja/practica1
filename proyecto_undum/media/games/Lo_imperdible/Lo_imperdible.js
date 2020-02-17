5ffee87479fbb9cdc766b9b1adb75a99df271a5e
David Padilla Montero <dpm00040@red.ujaen.es>
2020-02-17 11:53:08 +0100

    v1 finalizado

--- proyecto_undum/media/games/Lo_imperdible/Lo_imperdible.js
+++ proyecto_undum/media/games/Lo_imperdible/Lo_imperdible.js
@@ -100,7 +100,12 @@ undum.game.situations = {
 					system.setCharacterText( "<p>Aún soy jóven y de pequeño me dedicaba a escalar árboles, estoy seguro \
 					que desde el que esta al lado podría <a href='arbol' class='once'>saltar</a></p>" );
 				}
-	    }   
+	    },
+
+			enter: function( character, system, form ) {
+				system.setCharacterText("<p></p>");
+			
+			}	
 	  }
 	
 	),
@@ -109,7 +114,13 @@ undum.game.situations = {
 		y tendría que llamar a alguien, aunque no es que tuviese muchas opciones disponibles, el <a href='nollavesrespuesto'>casero</a> era una de ellas, por poco que me gustase.\
 		Sinó tendria que ver si al final Juan el <a href='nocambiacerradura'>cerrajero</a> me puede cojer el teléfono, porque no había demasiadas opciones disponibles.\
 		Llorar o patalear no iba a servir, aún podía escalar árboles, estoy seguro \
-		que desde el que esta al lado podría <a href='arbol' class='once'>saltar</a></</p>"
+		que desde el que esta al lado podría <a href='arbol' class='once'>saltar</a></</p>",
+		{
+			enter: function( character, system, form ) {
+				system.setCharacterText("<p></p>");
+			
+			}
+		}
 	),
 	arbol: new undum.SimpleSituation(
 		"<p>No iba a conseguir nada esperando, tenía que ponerme en marcha, así que me armé de valor.\
@@ -146,230 +157,11 @@ undum.game.situations = {
 		otras de repuesto de mi casero. Él me responde que no, no tiene otras, que si he perdido las llaves que asuma mi responsabilidad \
 		y <a href='cerrajerosi'>llame al cerrajero y le pague todo lo que le debo</a>. Pero pensándolo bien... y si en vez de pagarle, ¿me hago\
 		el valiente y me subo por el árbol <a href='arbol'>trepando?</a></p>"
-	),
-	
-	incorrecta01: new undum.SimpleSituation(
-	"<p>Tras buscar por largo rato entre tus pertenencias, con voz que transmite poca tranquilidad dices a los guardias de la puerta:</p>\
-	<p class='dialogo personaje'>- Fijaos que pertrechÃ© mi montura con toda suerte de abalorios, mÃ¡s olvideme el calculatorum...</p>\
-	<p>Ni que decir tiene, que los guardias pasan de ti y vuelven a su quehacer en el castillo.</p>\
-	<p>En fin... <a href='santoysenia'>puedes intentarlo de nuevo</a> o <a href='finalizar'>finalizar la partida</a>.</p>s"
-	),
-	
-	incorrecta02: new undum.SimpleSituation(
-	"<p>Aunque no tienes ni idea de quÃ© es una escuela, ni de si se come o no, intentas ser convincente mientras dices:</p>\
-	<p class='dialogo personaje'>- Fijaos que aun frecuentando continuamente las clases de maese VÃ­ctor, \
-	diose la circunstancia de que el dÃ­a que tal explicaron hallÃ¡bame cuidando de mi lobo huargo.</p>\
-	<p>Ni que decir tiene, que los guardias pasan de ti y vuelven a su quehacer en el castillo.</p>\
-	<p>En fin... <a href='santoysenia'>puedes intentarlo de nuevo</a> o <a href='finalizar'>finalizar la partida</a>.</p>"
-	),
-		
-	finalizar: new undum.SimpleSituation(
-	"<h1>Fin de la historia</h1>\
-	<p>Pues asÃ­ acaba tu historia en Roca Rivasly. QuizÃ¡, otro dÃ­a, con mÃ¡s tiempo, decidas acabarla del todo</p>\
-	<h1>Fin</h1>"
-	),
-	
-	patiodearmas: new undum.SimpleSituation(
-	"<h1>El patio de armas</h1>\
-	<p>EstÃ¡s dentro del castillo, concretamente en el patio de armas</p>\
-	<p>Los guardias se han quedado mÃ¡s tranquilos al saber que sabes el santo y seÃ±a, asÃ­ que se han ido a dormir... je, je, je...</p>\
-	<p>A tu izquierda, <a href='zonanoble'>puedes acceder a la zona noble del castillo</a>, \
-	mientras a la derecha observas que estÃ¡ <a href='caballerizas'>la entrada a las caballerizas</a>.</p>"
-	),
-	
-	zonanoble: new undum.SimpleSituation(
-	"<h1>La zona noble del castillo</h1>\
-	<p>En este ala de la fortaleza se desarrolla la vida palaciega, ya sabes: la cocina, el comedor, los aposentos, el gimnasio con spa\
-	y la terraza donde se ubican las placas solares... quiero decir, el solarium.</p>",
-	{
-		enter: function( character, system, from ) {
-			if( character.qualities.antorcha ) {
-				system.doLink( "zonanobleantorcha" );
-			} else {
-				system.write( "<p>No obstante, existe un pequeÃ±o problema y es que el arquitecto \
-				en un intento por llevar a cabo la mÃ¡xima expresiÃ³n del minimalismo, \
-				minimizÃ³ todas las ventanas (vamos: que se olvidÃ³ poner ventanas en las paredes)\
-				y dado que no tienes ninguna fuente de luz, no ves ni un pimiento.</p>\
-				<p>No tienes muchas opciones, salvo las de quedarte aquÃ­ esperando o bien <a href='patiodearmas'>salir al patio de armas</a>.</p>");
-				}
-			}
-		}
-	),
-
-	zonanobleantorcha: new undum.SimpleSituation( 
-					"<p>El insignificante error cometido por el arquitecto al no colocar ni una sola ventana\
-					no es problema para ti que te has procurado una antorcha.</p>\
-					<p class='transient'>Analizas la estancia y compruebas que existe una puerta en la pared norte\
-					<a href='aposentos'>que te lleva a los aposentos reales</a>.</p>\
-					<p class='transient'>Otra opciÃ³n es pasar del tema, que ya se te va haciendo tarde y <a href='patiodearmas'>volver por donde\
-					has venido</a>.</p>"
-		),
-		caballerizas: new undum.SimpleSituation(
-			"<h1>Las caballerizas</h1>\
-			<p class='dialogo'>- Â¿CuÃ¡nto son 3+4?</p>\
-			<p>El establo es el lugar ideal para encontrar caballos... Â¡Â¡y todo tipo de enseres!! \
-			Ya se sabe, empieza uno con el castillo bien ordenadito, y terminas guardando aquÃ­ todo lo que no te hace falta.</p>\
-			<p>No obstante, quizÃ¡ haya algo que te venga bien y que desees coger: \
-			una <a href='./silladeplaya' class='once'>silla de la playa</a>, un <a href='./spectrum' class='once'>Spectrum +2 128 megas</a>, \
-			una <a href='./antorcha' class='once'>antorcha</a>, un <a href='./latigo' class='once'>esplÃ©ndido lÃ¡tigo</a> de cuero negro, un <a href='./sombrero'  class='once'>sombrero mejicano</a> del anciano Ser Rodrick, el <a href='./tioserrodrick' class='once'>propio Ser Rodrick</a>...</p>\
-			<p>Esta estancia es grande, pero sÃ³lo tiene una puerta de entrada y salida por la que accedes, nuevamente, <a href='patiodearmas'>al patio de armas</a>.</p>\
-		",
-				{
-            actions: {
-                'silladeplaya': function( character, system, action) {
-					system.setCharacterText( "<p>Ahora ya llevas una silla de playa. \
-					Te vendrÃ¡ bien cuando encuentres...eh... una playa.</p>" );
-				},
-                'spectrum': function( character, system, action) {
-					system.setCharacterText( "<p>Â¡QuÃ© buenos salieron los Spectrum! \
-					Este todavÃ­a funciona.. de hecho, estÃ¡ sin estrenar.\
-					La ausencia de enchufes en esta Ã©poca podrÃ­a estar \
-					relacionada con tal estado.</p>" );
-				},
-                'latigo': function( character, system, action) {
-					system.setCharacterText( "<p>Â¡Un lÃ¡tigo puede salvarte de mil apuros! \
-					Aunque por desgracia, esta aventura no estÃ¡ ambientada en \
-					el personaje de Indiana Jones, por lo que te va a servir de bien poco.</p>" );
-				},
-                'sombrero': function( character, system, action) {
-					system.setCharacterText( "<p>Te quitas el yelmo y te colocas el sombrero mejicano.\
-					Sientes un irrefrenable deseo de cantar \
-					rancheras, pero sabes que sin tu yelmo \
-					eres mÃ¡s vulnerable al impacto de las hortalizas.</p>" );
-				},
-                'tioserrodrick': function( character, system, action) {
-					system.setCharacterText( "<p>La compaÃ±Ã­a de Ser Rodrick te harÃ¡ mÃ¡s agradable el camino...\
-					o no, teniendo en cuenta	que muriÃ³ hace mÃ¡s de 200 aÃ±os.</p>" );
-				},
-				'antorcha': function(character, system, action) {
-					system.setQuality( "antorcha", true );
-					system.setCharacterText("<p>Perfecto. Ahora tienes una antorcha con la que podrÃ¡s iluminar \
-					las oscuras estancias de la zona noble</p>");
-				}
-
-            }
-        }
-		),
-		habrey: new undum.SimpleSituation(
-			"<h1>El aposento de la izquierda</h1>\
-			<p>Tras subir las escaleras que quedaban a tu izquierda, accedes a uno de los aposentos... o mÃ¡s bien, a la puerta del mismo.\
-			Con tiento y cuidado, intentas hacer girar el picaporte y te das cuenta de que la puerta estÃ¡ cerrada con llave.</p>",
-			{
-				enter:function( character, system, from ) {
-					if( character.qualities.llave ) {
-						system.doLink( "habreyllave");
-					} else {
-						system.doLink( "habnollave");
-					}
-				}
-			}
-		),	
-		habnollave: new undum.SimpleSituation(
-			"<p><strong>Â¡Â¡LÃ¡stima que no tengas ninguna llave!!</strong></p>\
-			 <p>Pero una vez que has llegado hasta aquÃ­, no vas a cejar en tu empeÃ±o por unos mÃ­seros gramos de metal.</p>\
-			<p class='transient'>Tras meditarlo unos instantes, decides que tus opciones son <a href='./llamar-puerta' class='once'>llamar a la puerta de la habitaciÃ³n</a>\
-			(en plan <a href='http://www.youtube.com/watch?v=5_swaxOidGU' class='raw' target='_new'>Bob Dylan</a>),\
-			<a href='./punta-lanza'  class='once'>probar a abrir la cerradura</a> con la punta de tu lanza</a> (cuentan las leyendas \
-			que una vez hace tiempo a alguien le funcionÃ³), <a href='./empujar'  class='once'>dar un fuerte empujÃ³n a la puerta</a> cual si fueras Harry el sucio o <a href='aposentos'  class='once'>volver hacia atrÃ¡s sin hacer ruido</a>.</p>",
-			{
-				actions: {
-					'llamar-puerta': function( character, system, action ) {
-						system.write( '<p>Aunque intentas hacer el mÃ­nimo ruido posible, tus golpes en la puerta se escuchan en todo \
-														el castillo... de hecho, se escuchan en toda la comarca.</p>\
-														<p>SÃºbitamente (siempre quise decir esta palabra) aparecen cientos de miles de soldados de \
-														la guardia real, los cuales te arrestan y te llevan al calabozo donde dormirÃ¡s el sueÃ±o de los justos.</p>\
-													<p class="transient">Siento decirte que no tienes mÃ¡s opciÃ³n que \
-													<a href="calabozo">seguirlos al calabozo</a>.</p>' );
-					},
-					'punta-lanza': function( character, system, action ) {
-						system.write( '<p>Sin tener en cuenta que nunca has intentado, ni por tanto conseguido, abrir una cerradura con tu lanza\
-													te lanzas (nunca mejor dicho) a la ardua tarea de romper el cÃ³digo por el mÃ©todo de la fuerza bruta.</p> \
-													<p>Tras apenas dos horas de esfuerzo, te das cuenta de que no lo vas a conseguir, y desistes del plan.</p>\
-													<p>Por desgracia para ti, los sensores de movimiento de lanzas del castillo han dado la alerta a la guardia \
-													real, que hace cosa asÃ­ de 30 minutos que estÃ¡ partiÃ©ndose de risa viendo como te peleas con la cerradura.</p>\
-													<p>Finalmente, se impone su profesionalidad, y te arrestan llevÃ¡ndote al calabozo, donde podrÃ¡s seguir\
-													practicando el arte de la criptolanzografÃ­a...vamos, intentando abrir cerraduras con lanzas.</p>\
-													<p class="transient">Siento decirte que no tienes mÃ¡s opciÃ³n que \
-													<a href="calabozo">seguirlos al calabozo</a>.</p>' );
-					},
-					'empujar': function( character, system, action ) {
-						system.write( '<p>Con ardoroso Ã­mpetu e impetuoso ardor, das seis pasos hacia atrÃ¡s, abres decididamente las piernas, \
-						y colocas los brazos pegados a los costados con los puÃ±os apretados hacia abajo, mientras miras fijamente la pelota... \
-						eh... la puerta.</p>\
-						<p>Al grito de \'Viva mi madre\' te lanzas contra la puerta con la certeza de que en breves instantes el duro e \
-						impenetrable material quedarÃ¡ reducido a aÃ±icos...<strong>Â¡Â¡Y asÃ­ es!!</strong>... al menos para los huesos \
-						que forman tu hombro, brazo y antebrazo, porque lo que es la puerta apenas si consigue un araÃ±azo.</p>\
-						<p>El enorme estruendo que has formado, sÃ³lo superado por el estentÃ³reo grito con el que has iniciado la carga, \
-						pone en alerta al numeroso ejÃ©rcito de la guardia real que, como por encantamiendo, surge para apresarte y \
-						llevarte al calabozo, donde dispondrÃ¡s de toda una vida para pensar cÃ³mo es posible que una puerta de roble macizo, \
-						con marco de hierro y atrancada tanto por dentro como por fuera no haya cedido ante tu furioso ataque.</p>\
-						<p class="transient">Siento decirte que no tienes mÃ¡s opciÃ³n que \
-						<a href="calabozo">seguirlos al calabozo</a>.</p>' );
-					}
-
-				}
-			}
-		),
-
-		habprincesallave: new undum.SimpleSituation(
-			"<p>Con suma destreza, gran sigilo y la llave que te agenciaste, consigues abrir la puerta y entras cautelosamente en la estancia.</p>\
-			<p>Al fondo, a la derecha, observas que hay un lecho en el que alguien duerme placenteramente; te acercas\
-			<a href='./discurso' class='once'> e inicias el pequeÃ±o\
-			discurso</a> que tantos meses llevas preparando:</p>",
-			{
-				actions: {
-					'discurso': function( character, system, action ) {
-						system.write( "<p class='dialogo personaje'>- Â¡La princesa estÃ¡ dormida! Â¿A quÃ© hora se habrÃ¡ acostado la princesa?<br/>\
-			Â¡Mi bien, mi sol, mi estrella, mi planeta, mi satÃ©lite, mi polvo espacial, mi agujero negro, mi bosÃ³n de Higgs!\<br/>\
-			Si de tu dulce boca a bien tuvieras susurrar palabras que alentaran el corazÃ³n de este, vuestro humilde servidor, \
-			ante los antiguos dioses y tambiÃ©n ante los modernos, amor sin par os jurarÃ­a. Un amor puro, cual no hubo ni habrÃ¡; \
-			un amor del cual se forjarÃ¡n leyendas; un amor que durarÃ¡ toda la eternidad, o en su defecto hasta que amanezca.<br/>\
-			Decidme, mi pequeÃ±o pastel de fresa con guinda de chocolate: Â¿me amÃ¡is?</p>\
-			<p>Al tiempo que terminas tu discurso, una enorme figura de 150 kilos se gira en la cama, y su barbuda cara se\
-			acerca a la tuya para decir:</p>\
-			<p class='dialogo'><strong>- Â¡Â¡Eres tÃº mi prÃ­ncipe azul que yo soÃ±Ã©!!</strong></p>\
-			<p>Efectivamente: has acertado en la habitaciÃ³n de la princesa; no obstante y asÃ­, como el que no quiere la cosa, \
-			recuerdas que habÃ­as quedado con los seÃ±ores de OtoÃ±olandia para jugaros vuestras tierras al padel.</p>\
-			<p>De esa forma, antes de que la princesa pueda seguir con su grÃ¡cil canto, le sueltas de golpe y porrazo toda la \
-			teorÃ­a que aprendiste en clase de ProgramaciÃ³n Orientada a Objetos consiguiendo que tu bella dama caiga de nuevo \
-			en brazos de Morfeo... el cual a duras penas puede sujetarla. Mientras ella duerme, tÃº sales precipitadamente del castillo.</p>");
-						system.doLink( 'happyend' );
-					} 
-				}
-			}),
-		
-		happyend: new undum.SimpleSituation(
-			"<h1>Final feliz</h1>\
-			<p>Y asÃ­ acaba esta caballeresca aventura: entraste al castillo, recuperaste los objetos necesarios y juraste tu amor \
-			eterno a la hermosa... a la dama...</p>\
-			<p>Tu heroica figura avanza hacia el poniente, mientras el anaranjado sol desdibuja sombras pÃºrpuras a tu espalda y \
-			entonces comprendes a quÃ© hacen referencia cuando hablan de la soledad del poder. En este caso, no obstante, tÃº \
-			agradeces el poder volver en soledad.</p>\
-			<p>En realidad, el camino del castillo deberÃ­a estar jalonado de promociones de chalets y urbanizaciones con piscina, pero \
-			debido a un pequeÃ±o error del arquitecto ... bla, bla, bla.</p>\
-			<h1>Â¡Fin</h1>"
-		)
+	)
 };
 
 
 // Ejemplo de declaraciÃ³n separada
-undum.game.situations["calabozo"]=new undum.SimpleSituation(
-			"<h1>Calabozo</h1>\
-			<p>En el proyecto original del arquitecto, el calabozo era con diferencia la sala mÃ¡s grande del castillo.</p>\
-			Estaba formado por tres estancias comunicadas entre si por aperturas diÃ¡fanas. La primera estancia contenÃ­a una\
-			completa cocina, con dos ambientes separados que distinguÃ­an la zona de trabajo del <em>living</em>.\
-			La segunda, aÃºn mayor, era el recinto seleccionado para incluir un baÃ±o completo con jacuzzi, sauna turca y ducha\
-			de aromas. Finalmente, la tercera, albergaba una habitaciÃ³n de estilo colonial cuya principal \
-			joya era la cÃ³moda cama <em>king size</em>, autocalefactable.</p>\
-			<p>Desgraciadamente, en un pequeÃ±o despiste del ayudante del arquitecto, los planos dieron a parar al foso de los cocodrilos.\
-			Por mÃ¡s que el pobre muchacho se afanÃ³ en convencer a los reptiles de que soltaran de sus fauces los importantes pergaminos,\
-			el resultado fue nulo.</p>\
-			<p>Finalmente, se optÃ³ por hacer un profundo agujero en la tierra que hiciera las funciones de calabozo. \
-			Por supuesto, era una soluciÃ³n temporal... por lo que es lo Ãºnico del castillo que sigue intacto tras varios siglos.</p>\
-			<p>Por tanto aquÃ­ acaba tu aventura. Fue un notable intento, pero has acabado con tus huesos \
-			en el calabozo por los siglos de los siglos.</p>\
-			<h1>Fin</h1>"
-		);
 
 // ---------------------------------------------------------------------------
 /* The Id of the starting situation. */
@@ -380,12 +172,6 @@ undum.game.start = "start";
  * possess. We don't have to be exhaustive, but if we miss one out then
  * that quality will never show up in the character bar in the UI. */
 undum.game.qualities = {
-    antorcha: new undum.OnOffQuality(
-        "Antorcha", {priority:"0001", group:'inventario', onDisplay:"&#10003;"}
-    ),
-	  llave: new undum.OnOffQuality(
-        "Llave", {priority:"0002", group:'inventario', onDisplay:"&#10003;"}
-    )
 };
 
 // ---------------------------------------------------------------------------
@@ -402,8 +188,6 @@ undum.game.qualityGroups = {
 /* This function gets run before the game begins. It is normally used
  * to configure the character at the start of play. */
 undum.game.init = function(character, system) {
-    system.setQuality( "antorcha" , false )
-    system.setQuality( "llave" , false )
     system.setCharacterText("<p> </p>");
 };
 
